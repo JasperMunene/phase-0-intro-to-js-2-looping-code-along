ae5e4c998f2b9f749b737230d9aa296fd51cf943
JasperMunene <jaspermunene2022@gmail.com>
2024-09-21 09:39:17 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  const cards = [];
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    cards.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  return cards;
+}
+
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
